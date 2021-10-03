6cbeac756f37fe4a1aa5e05807b177c0b12ae559
stevengm45 <stevengm45@gmail.com>
2021-10-03 13:53:52 -0500

    fix code square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' im here again '''
+
+
+class Square():
+    """Description"""
+
+    def __init__(self, *args, **kwargs):
+        """Description"""
+        self.width = 0
+        self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """Description"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """Description"""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiterOfMySquare())
